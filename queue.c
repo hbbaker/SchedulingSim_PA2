f605e3567a72161c66e91d660955b595cff5124d
Henry <henrybbaker20@gmail.com>
2025-03-19 17:18:22 -0700

    added beginnings of a queue

---
+++ queue.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+#define MAX_SIZE 4
+
+typedef struct
+{
+    int size;
+    int front;
+    int rear;
+    int pids[MAX_SIZE];
+} Queue;
+
+void initQueue(Queue *queue)
+{
+    queue->front = -1;
+    queue->rear = -1;
+    queue->size = 0;
+}
+
+int getSize(Queue *queue)
+{
+    return queue->size;
+}
+
+int isFull(Queue *queue)
+{
+    return queue->size == MAX_SIZE;
+}
+
+int isEmpty(Queue *queue)
+{
+    return queue->size == 0;
+}
+
+int enqueue(Queue *queue, int pid)
+{
+    if (isFull(queue))
+    {
+        return -1;
+    }
+
+    if (isEmpty(queue))
+    {
+        queue->front = 0;
+    }
+
+    queue->rear = (queue->rear + 1) % MAX_SIZE;
+    queue->pids[queue->rear] = pid;
+    queue->size++;
+
+    return 0;
+}
